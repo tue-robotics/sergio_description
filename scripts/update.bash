1482f4eb8a869e447a611760fabc6513e62ffd4e
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-09-01 20:37:28 +0000

    Worked on simulation model
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo2_common/amigo2_description@18335 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ scripts/update.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+ROBOT_DESCRIPTION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+
+# Generate urdf from xacro
+rosrun xacro xacro.py -o $ROBOT_DESCRIPTION_DIR/urdf/amigo2.urdf $ROBOT_DESCRIPTION_DIR/urdf/xacro/amigo2.urdf.xacro
+
+# Generate sdf from urdf
+gzsdf print $ROBOT_DESCRIPTION_DIR/urdf/amigo2.urdf > $ROBOT_DESCRIPTION_DIR/sdf/amigo2.sdf
+
+grep 'Error' $ROBOT_DESCRIPTION_DIR/sdf/amigo2.sdf
+grep 'Warning' $ROBOT_DESCRIPTION_DIR/sdf/amigo2.sdf
+
+# Remove warning messages from sdf
+sed -i '/Warning/d' $ROBOT_DESCRIPTION_DIR/sdf/amigo2.sdf
+
+# Fix media paths
+sed -i 's/model:\/\/amigo_description\/media/model:\/\/media/g' $ROBOT_DESCRIPTION_DIR/sdf/amigo2.sdf
+
